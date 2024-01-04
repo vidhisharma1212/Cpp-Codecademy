3463e4a56e990998d9f12d9b702aeba92509407f
vidhisharma1212 <85745770+vidhisharma1212@users.noreply.github.com>
2024-01-04 16:00:00 +0530

    Create ufo_functions.hpp

---
+++ ufo_functions.hpp
@@ -0,0 +1,6 @@
+#include <vector>
+void greet();
+void display_status(std::string answer, std::string codeword, std::vector<char> incorrect);
+void end_game(std::string answer, std::string codeword);
+
+void display_misses(int misses);
